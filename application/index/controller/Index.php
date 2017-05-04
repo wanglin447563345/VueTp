c56d02056f1cacd927a0c9ea70b3867a4b1d8be2
tbenben <1558970807@qq.com>
2017-05-04 16:10:32 +0800

    sss

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,10 +5,6 @@ class Index
 {
     public function index()
     {
-<<<<<<< HEAD
-        return '谭鸡鸡qsd鸡鸡';
-=======
-        return '谭谭鸡鸡';
->>>>>>> d3ecb937c824b029967e2de088e6d963248da94d
+        return 'aadsfqsd鸡鸡';
     }
 }
